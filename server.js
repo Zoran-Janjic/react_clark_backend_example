ac6ff73e7d7baca58bcd247098b820f4aa5b868c
ZoranJanjic <zo-janjic@gl-navi.co.jp>
2024-04-27 19:46:51 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { Webhook } from "svix";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const expressApp = express();
+const port = process.env.PORT || 7000;
+
+expressApp.post(
+  "/api/webhooks",
+  bodyParser.raw({ type: "application/json" }),
+  (req, res) => {
+    try {
+      const payloadString = req.body.toString();
+      const svixHeaders = req.headers;
+
+      const WEBHOOK_SECRET = process.env.CLERK_SIGNING_SECRET;
+      if (!WEBHOOK_SECRET) {
+        throw new Error("You need a WEBHOOK_SECRET in your .env");
+      }
+
+      const webhook = new Webhook(WEBHOOK_SECRET);
+
+      const event = webhook.verify(payloadString, svixHeaders);
+
+      const { id, ...otherAttributes } = event.data;
+
+      if (event.type === "webhook") {
+        console.log(`User is ${id} is ${event.type}`);
+        console.log(`Other atrributes int the event ${otherAttributes}`);
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Webhook verified",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+);
+
+expressApp.listen(port, () => {
+  console.log(`Listening on: ${port}`);
+});
